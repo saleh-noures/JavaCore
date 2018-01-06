868f324b90c13c0e5c9bfd5474f8e49a3d24210e
Saleh Noures <saleh.noures@au.fujitsu.com>
2018-01-06 16:57:03 +1100

    Change ToDo Message

--- src/Multithreading/MT8_Next_WaitAndNotify.java
+++ src/Multithreading/MT8_Next_WaitAndNotify.java
@@ -4,7 +4,7 @@ public class MT8_Next_WaitAndNotify {
 
 	//wait, notify, and notifyAll are used to allow threads to communicate 
 	public static void main(String[] args) {
-		// TODO Create an example
+		// TODO: Write An Example
 
 	}
 
